f35fb069cfd65e554ba94ef49d61ba371a4cad90
Vladimir Kornea <vladkornea@gmail.com>
2018-10-11 22:12:44 -0400

    Make the login page redirect to the home page if the user is already logged in.

--- pages/login/index.php
+++ pages/login/index.php
@@ -1,4 +1,10 @@
 <?php
 
-require $_SERVER['DOCUMENT_ROOT'] .'/index.php';
+require_once $_SERVER['DOCUMENT_ROOT'] .'/includes/config.php';
+
+if (Session::getUserId()) {
+	HttpPageShell::redirect('/');
+} else {
+	require $_SERVER['DOCUMENT_ROOT'] .'/index.php';
+}
 
