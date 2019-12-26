e8912a0cb84d37aa684ee7fca16a9e4a9ad38713
Vladimir Kornea <vladkornea@gmail.com>
2019-12-26 17:08:00 -0500

    Profile page - change "initiate contact" to "initiate first contact"

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -130,7 +130,7 @@ function printProfilePageInterface () {
 			$form.find('#contact-form-instructions').text("You can send a new message " +nextAllowedSendFormatted)
 		} else {
 			if (!userPreviouslyContacted) {
-				$form.find('#contact-form-instructions').text("You can only initiate contact once per day, so make your messages count.")
+				$form.find('#contact-form-instructions').text("You can only initiate first contact once per day, so make your messages count.")
 			} else {
 				$form.find('#contact-form-instructions').text("Conversation messages are listed latest-first.")
 			}
