7532936dc0d708b3d6f1ffff565979186cb62d58
Vladimir Kornea <vladkornea@gmail.com>
2020-01-14 14:49:15 -0500

    update privacy policy

--- pages/privacy-policy/index.php
+++ pages/privacy-policy/index.php
@@ -9,10 +9,11 @@ $pageShell->addCssFile('/pages/privacy-policy/privacy-policy.css');
 
 Only logged-in users can see your profile.
 
-You can <a href="/account">deactivate your profile</a> at any time.
+<a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">HTTP cookies</a> are used to keep you logged in.
+
+<a href="https://policies.google.com/technologies/partner-sites" target="_blank">Google Analytics</a> is utilized unless "<a href="https://en.wikipedia.org/wiki/Do_Not_Track" target="_blank">Do Not Track</a>" is requested.
 
 Passwords are <a href="http://php.net/manual/en/function.password-hash.php" target="_blank">not stored in a retrievable format</a>.
 
-The web server requires secure <a href="https://en.wikipedia.org/wiki/HTTPS" target="_blank">HTTPS</a> encryption for all requests.
-
-Running on Amazon Web Services (AWS).</div>
+<a href="https://en.wikipedia.org/wiki/HTTPS" target="_blank">HTTPS</a> encryption required for all requests.
+</div>
