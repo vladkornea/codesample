3fa1933f0b5f6ced621ec7a6e86091d4ecc9ec88
Vladimir Kornea <vladkornea@gmail.com>
2020-01-15 11:07:28 -0500

    update privacy policy

--- pages/privacy-policy/index.php
+++ pages/privacy-policy/index.php
@@ -5,10 +5,35 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/includes/config.php';
 $pageShell = new StandardPageShell("Privacy Policy");
 $pageShell->addCssFile('/pages/privacy-policy/privacy-policy.css');
 ?>
-<div class="text" id="privacy-policy-text">
-<a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">HTTP cookies</a> are used to keep you logged in, and by <a href="https://policies.google.com/technologies/partner-sites" target="_blank">Google Analytics</a> unless "<a href="https://en.wikipedia.org/wiki/Do_Not_Track" target="_blank">Do Not Track</a>" is requested.
+<div style="width:6.5in; text-align:justify; line-height:1.6em;">
+	<h3>Cookies</h3>
 
-Passwords are <a href="http://php.net/manual/en/function.password-hash.php" target="_blank">not stored in a retrievable format</a>. Email addresses are kept private. Other personal information, such as city and birthday, are useful for TypeTango searches; only logged-in users can see or deduce some of this information. Personal information will not be sold or shared with third parties; TypeTango publishes some anonymous statistics, such as the user type distribution.
+	<p>TypeTango uses <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">HTTP cookies</a> to keep you logged in.</p>
 
-<a href="https://en.wikipedia.org/wiki/HTTPS" target="_blank">HTTPS</a> encryption is required for all requests.
+	<p>TypeTango also uses <a href="https://policies.google.com/technologies/partner-sites" target="_blank">Google Analytics</a> to track website traffic. <a href="https://policies.google.com/technologies/partner-sites" target="_blank">Google Analytics</a> uses <a href="https://en.wikipedia.org/wiki/HTTP_cookie#Privacy_and_third-party_cookies" target="_blank">third-party cookies</a> as explained in <a href="https://policies.google.com/technologies/partner-sites" target="_blank">Google's privacy policy</a>.</p>
+
+	<h3>"Do Not Track" header</h3>
+
+	<p>If your browser includes a "<a href="https://en.wikipedia.org/wiki/Do_Not_Track" target="_blank">Do Not Track</a>" HTTP header (like during <a href="https://en.wikipedia.org/wiki/Private_browsing" target="_blank">private browsing</a>), TypeTango will not include <a href="https://policies.google.com/technologies/partner-sites" target="_blank">Google Analytics</a>.</p>
+
+	<h3>Registration</h3>
+
+	<p>Passwords are <a href="http://php.net/manual/en/function.password-hash.php" target="_blank">not stored in a retrievable format</a>. Email addresses are kept private. Your date of birth is used to calculate your age (which is used in TypeTango searches), but is not shown to other users. Other personal information like your city and gender are requested in order to show to other users.</p>
+
+	<h3>Sharing</h3>
+
+	<p>Personal information will not be sold or shared with third parties. TypeTango publishes some anonymous statistics, such as type distribution of users.</p>
+
+	<h3>Data Location</h3>
+
+	<p>Running on <a href="https://aws.amazon.com/" target="_blank">Amazon Web Services</a> in Northern Virginia in the United States of America.</p>
+
+	<h3>Contact Us</h3>
+
+	<p>Send email to owner at typetango.com</p>
+
+	<p>TypeTango<br>
+		70-22 66th St 3L<br>
+		Glendale, NY 11385</p>
+
 </div>
