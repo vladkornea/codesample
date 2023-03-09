0a044d7a4588ccd4b3a0fc6111e856a9e75deccd
Vladimir Kornea <vladkornea@gmail.com>
2023-03-09 14:15:44 -0500

    add schema to recommended books on help page

--- pages/help/index.php
+++ pages/help/index.php
@@ -22,12 +22,76 @@ $pageShell->addCssFile('/pages/help/help.css');
 <p>TypeTango sends an email to the person you are attempting to contact.</p>
 
 <h3>Books</h3>
-	<ul>
-<li><a rel="external nofollow" href="https://www.amazon.com/exec/obidos/tg/detail/-/089106074X" target="_blank"><i>Gifts Differing: Understanding Personality Type</i></a> by Isabel Briggs Myers, Peter B. Myers<br>Good introduction to the entire theory, along with statistical data</li>
-<li><a rel="external nofollow" href="https://www.amazon.com/exec/obidos/tg/detail/-/0877739870" target="_blank"><i>Personality Type: An Owner's Manual</i></a> by Lenore Thomson<br>Functions in their dominant role</li>
-<li><a rel="external nofollow" href="https://www.amazon.com/exec/obidos/tg/detail/-/0891061703" target="_blank"><i>Was That Really Me?: How Everyday Stress Brings Out Our Hidden Personality</i></a> by Naomi L. Quenk<br>Functions in their inferior role</li>
-<li><a rel="external nofollow" href="https://www.amazon.com/exec/obidos/tg/detail/-/1885705026" target="_blank"><i>Please Understand Me II: Temperament, Character, Intelligence</i></a> by David Keirsey<br>Categorizes the sixteen types into four groups (SJ Guardians, SP Artisans, NF Idealists, NT Rationals).</li>
-</ul>
+<ul><li itemscope itemtype="https://schema.org/Book">
+	<a itemprop="name" rel="external nofollow" href="https://www.amazon.com/exec/obidos/tg/detail/-/089106074X" target="_blank">
+		Gifts Differing: Understanding Personality Type
+	</a>
+	<div>
+		by <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+			<a itemprop="name" rel="external nofollow" href="https://en.wikipedia.org/wiki/Isabel_Briggs_Myers" target="_blank">
+				Isabel Briggs Myers
+			</a>
+		</span>
+		and <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+			<a itemprop="name" rel="external nofollow" href="https://www.goodreads.com/author/show/434031.Peter_B_Myers" target="_blank">
+				Peter B. Myers
+			</a>
+		</span>
+	</div>
+	<div itemprop="review" itemscope itemtype="https://schema.org/Review">
+		<span itemprop="reviewBody">
+			Good introduction to the entire theory, along with statistical data.
+		</span>
+	</div>
+</li><li itemscope itemtype="https://schema.org/Book">
+	<a itemprop="name" rel="external nofollow" href="https://www.amazon.com/exec/obidos/tg/detail/-/0877739870" target="_blank">
+		Personality Type: An Owner's Manual
+	</a>
+	<div>
+		by <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+			<a itemprop="name" rel="external nofollow" href="https://www.goodreads.com/author/show/302550.Lenore_Thomson" target="_blank">
+				Lenore Thomson
+			</a>
+		</span>
+	</div>
+	<div itemprop="review" itemscope itemtype="https://schema.org/Review">
+		<span itemprop="reviewBody">
+			Functions in their dominant role.
+		</span>
+	</div>
+</li><li itemscope itemtype="https://schema.org/Book">
+	<a itemprop="name" rel="external nofollow" href="https://www.amazon.com/exec/obidos/tg/detail/-/0891061703" target="_blank">
+		Was That Really Me?: How Everyday Stress Brings Out Our Hidden Personality
+	</a>
+	<div>
+		by <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+			<a itemprop="name" rel="external nofollow" href="https://www.goodreads.com/author/show/433616.Naomi_L_Quenk" target="_blank">
+				Naomi L. Quenk
+			</a>
+		</span>
+	</div>
+	<div itemprop="review" itemscope itemtype="https://schema.org/Review">
+		<span itemprop="reviewBody">
+			Functions in their inferior role.
+		</span>
+	</div>
+</li><li itemscope itemtype="https://schema.org/Book">
+	<a itemprop="name" rel="external nofollow" href="https://www.amazon.com/exec/obidos/tg/detail/-/1885705026" target="_blank">
+		Please Understand Me II: Temperament, Character, Intelligence
+	</a>
+	<div>
+		by <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+			<a itemprop="name" rel="external nofollow" href="https://en.wikipedia.org/wiki/David_Keirsey" target="_blank">
+				David Keirsey
+			</a>
+		</span>
+	</div>
+	<div itemprop="review" itemscope itemtype="https://schema.org/Review">
+		<span itemprop="reviewBody">
+			Categorizes the sixteen types into <abbr title="SJ types (ESFJ, ESTJ, ISFJ, ISTJ)">Guardians</abbr>, <abbr title="SP types (ESFP, ESTP, ISFP, ISTP)">Artisans</abbr>, <abbr title="NF types (ENFJ, ENFP, INFJ, INFP)">Idealists</abbr>, and <abbr title="NT types (ENTJ, ENTP, INTJ, INTP)">Rationals</abbr>.
+		</span>
+	</div>
+</li></ul>
 
 <h3>Contact Us</h3>
 <p>Send an email to owner at <?=EMAIL_DOMAIN?></p>
